8ebc2db288d6959a3df4c08ecf3bd9e374ba1f21
becem-gharbi <becem.gharbi@live.com>
2024-04-30 20:03:10 +0100

    types: fix $peerjs

--- src/runtime/types/index.d.ts
+++ src/runtime/types/index.d.ts
@@ -1,3 +1,16 @@
+import type { Peer } from 'peerjs'
+
+declare module '#app' {
+  interface NuxtApp {
+    $peerjs: {
+      init: (uid: string) => void
+      end: () => void
+      connected: Ref<boolean>
+      readonly peer: Peer | null
+    }
+  }
+}
+
 export interface PublicConfig {
   host: string
   path: string
