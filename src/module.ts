03fc71b1f42a9fe777158ff0d7001f8525d1105a
becem-gharbi <becem.gharbi@live.com>
2024-04-30 08:40:37 +0100

    chore: pass runtime  config options

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,11 @@
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defu } from 'defu'
 
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  host: string
+  path: string
+  port: number
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -8,11 +13,26 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: 'peerjs',
   },
 
-  defaults: {},
+  defaults: {
+    host: '0.peerjs.com',
+    path: '/',
+    port: 443,
+  },
 
   setup(_options, _nuxt) {
     const { resolve } = createResolver(import.meta.url)
 
     addPlugin(resolve('./runtime/plugin'))
+
+    _nuxt.options.runtimeConfig = defu(_nuxt.options.runtimeConfig, {
+      app: {},
+      public: {
+        peerjs: {
+          host: _options.host,
+          path: _options.path,
+          port: _options.port,
+        },
+      },
+    })
   },
 })
