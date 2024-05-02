8398b75e47386da276eeda57ab3a867c797f1701
becem-gharbi <becem.gharbi@live.com>
2024-05-02 16:43:35 +0100

    feat: implement data APIs

--- src/runtime/utils/NPeerjs.ts
+++ src/runtime/utils/NPeerjs.ts
@@ -4,15 +4,16 @@ import type { Ref } from '#imports'
 
 type Connection = 'online' | 'offline'
 type Media = 'active' | 'inactive' | 'calling' | 'waiting'
-type Data = object | string
 
 export class NPeer {
   id: string
   static peer: Peer
 
   connection: Ref<Connection>
-  #rmDataConnection: DataConnection | null
+  rmDataConnection: DataConnection | null
   #lcDataConnection: DataConnection | null
+  #connectInterval: NodeJS.Timeout | null
+  #connectIntervalMs: number
 
   static media: Ref<Media>
   static #rmMediaConnection: MediaConnection | null
@@ -24,8 +25,16 @@ export class NPeer {
     NPeer.peer = peer
 
     this.connection = useState<Connection>(`peer:${id}:connection`, () => 'offline')
-    this.#rmDataConnection = null
+    this.rmDataConnection = null
     this.#lcDataConnection = null
+    this.#connectInterval = null
+    this.#connectIntervalMs = 5000
+
+    NPeer.peer.on('connection', (connection) => {
+      if (connection.label === this.id) {
+        this.#lcDataConnection = connection
+      }
+    })
 
     NPeer.media = useState<Media>(`peer:media`, () => 'inactive')
     NPeer.#rmMediaConnection = null
@@ -33,10 +42,36 @@ export class NPeer {
     NPeer.#lcMediaStream = null
   }
 
-  connect(opts?: PeerConnectOption) { }
-  disconnect() { }
-  sendData(data: Data) { }
-  onData(cb: (data: Data) => void) {}
+  connect(opts?: PeerConnectOption) {
+    this.#connectInterval = setInterval(() => {
+      if (!NPeer.peer.disconnected && !this.connection.value) {
+        this.rmDataConnection = NPeer.peer.connect(this.id, {
+          ...opts,
+          label: this.id,
+        })
+
+        this.rmDataConnection.on('open', () => {
+          this.connection.value = 'online'
+        })
+        this.rmDataConnection.on('close', () => {
+          this.connection.value = 'offline'
+        })
+      }
+    }, this.#connectIntervalMs)
+  }
+
+  disconnect() {
+    if (this.#connectInterval) {
+      clearInterval(this.#connectInterval)
+      this.#connectInterval = null
+    }
+    this.rmDataConnection?.close()
+    this.#lcDataConnection?.close()
+  }
+
+  async sendData(data: unknown, chunked?: boolean) {
+    await this.#lcDataConnection?.send(data, chunked)
+  }
 
   startCall(rmVideoEl: HTMLVideoElement, opts?: CallOption) { }
   static onCall(rmVideoEl: HTMLVideoElement, cb: () => void) {}
